b0d9356ae85a8097b5d0a9d52bae60bb10e98d31
Alessio Montuoro <alessio@montuoro.info>
2022-04-09 09:13:56 +0200

    added examples

--- encrypt_3d/config.py
+++ encrypt_3d/config.py
@@ -3,9 +3,9 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     key: int = 0
-    grid_width: int = 32
-    grid_height: int = 16
-    hole_ratio: float = 0.25
+    grid_width: int = 8
+    grid_height: int = 8
+    hole_ratio: float = 0.2
 
 
 cfg = Settings()
